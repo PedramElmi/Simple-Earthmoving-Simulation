1fb1a21223c51cb5c711aaa6403f643e7723ec3b
Pedram Elmi <61834096+PedramElmi@users.noreply.github.com>
2020-11-26 15:33:17 +0330

    Update (naming convention pep 8)

--- program.py
+++ program.py
@@ -1,26 +1,30 @@
 class SimulationTime:
     type = "Real Simulation"
     now = 0
-    
-    def Add(duration):
+
+    def add(self, duration):
         SimulationTime.now += duration
         return SimulationTime.now
-    def ResetVirtual(duration):
+
+    def reset_virtual(self, duration):
         SimulationTime.now -= duration
-    
+
+
 class BranchTime(SimulationTime):
     type = "Branch"
-    def Add(duration):
+
+    def add(self, duration):
         BranchTime.now += duration
         return BranchTime.now
-    
-    def ResetVirtual(_):
+
+    def reset_virtual(self, duration):
         BranchTime.now = SimulationTime.now
 
+
 class Loader:
-    
-    def __init__(self, loaderStatus):
-        self.status=loaderStatus
+
+    def __init__(self, loader_status):
+        self.status = loader_status
 
     # def Working(self):
     #     """
@@ -29,148 +33,106 @@ class Loader:
     #     print("Loader {} started working at {}".format(self.id,))
     #     print("Truck {} Finished working at {}".format(self.id))
 
+
 class Truck:
-    
-    def __init__(self, ID, loadingDur, haulingDur, dumpingDur, returningDur):
-        self.id = ID
-        self.loadingDuration = loadingDur
-        self.haulingDuration = haulingDur
-        self.dumpingDuration = dumpingDur
-        self.returningDuration = returningDur
+
+    def __init__(self, truck_id, loading_dur, hauling_dur, dumping_dur, returning_dur):
+        self.id = truck_id
+        self.loading_duration = loading_dur
+        self.hauling_duration = hauling_dur
+        self.dumping_duration = dumping_dur
+        self.returning_duration = returning_dur
         self.status = "cycle starting"
-    
-    def Process(self,TimeClass,loader):
+
+    def process(self, TimeClass, loader):
         if self.status == "cycle starting":
-            return self.Loading(TimeClass,loader)
-            
+            return self.loading(TimeClass, loader)
 
-
-    def Loading(self,TimeClass,loader):
+    def loading(self, TimeClass, loader):
         """
         docstring
         """
         if loader.status == "idle":
-            
-            print(TimeClass.type,"Truck {} started Loading at {}".format(self.id,TimeClass.now))
+
+            print(TimeClass.type, "Truck {} started Loading at {}".format(self.id, TimeClass.now))
             startTime = TimeClass.now
-            TimeClass.Add(self.loadingDuration)
-            print(TimeClass.type,"Truck {} Finished Loading at {}".format(self.id,TimeClass.now))
+            TimeClass.add(self.loading_duration)
+            print(TimeClass.type, "Truck {} Finished Loading at {}".format(self.id, TimeClass.now))
             finishTime = TimeClass.now
-            
+
             if TimeClass == SimulationTime:
                 loader.status = "active"
                 self.status = "idle"
-            TimeClass.ResetVirtual(self.loadingDuration)
-            return (startTime, finishTime, self.id, "Loading") 
+            TimeClass.reset_virtual(self.loading_duration)
+            return startTime, finishTime, self.id, "Loading"
         else:
-            print(TimeClass.type,"Truck {} couldnt start loading at {}".format(self.id,TimeClass.now))
+            print(TimeClass.type, "Truck {} could not start loading at {}".format(self.id, TimeClass.now))
 
     # def Hauling(self):
     #     """
     #     docstring
     #     """
-        
+
     #     print("Truck {} started Hauling at {}".format(self.id, time))
     #     time += self.haulingDuration
     #     print("Truck {} Finished Hauling at {}".format(self.id, time))
-
-
 
     # def Dumping(self):
     #     """
     #     docstring
     #     """
-        
+
     #     print("Truck {} started Dumping at {}".format(self.id, time))
     #     time += self.dumpingDuration
     #     print("Truck {} Finished Dumping at {}".format(self.id, time))
-        
 
     # def Returning(self):
     #     """
     #     docstring
     #     """
-        
+
     #     print("Truck {} started Returning at {}".format(self.id, time))
     #     time += self.returningDuration
-    #     print("Truck {} Finished Returninging at {}".format(self.id, time))
-
-
-# loadingDuration=10
-# haulingDuration=10
-# dumpingDuration=3
-# returninDduration=5
-
-# myTruckList=[]
-# for i in range(quantity):
-#     myTruckList.append(Truck(ID=i,
-#                 loadingDur=loadingDuration,
-#                 haulingDur=haulingDuration,
-#                 dumpingDur=dumpingDuration,
-#                 returningDur=returninDduration))
+    #     print("Truck {} Finished Returning at {}".format(self.id, time))
 
 
 a0 = Truck(
-        ID=0,
-        loadingDur=10,
-        haulingDur=20,
-        dumpingDur=7,
-        returningDur=15)
-
+    truck_id=0,
+    loading_dur=10,
+    hauling_dur=20,
+    dumping_dur=7,
+    returning_dur=15)
 
 a1 = Truck(
-        ID=1,
-        loadingDur=15,
-        haulingDur=30,
-        dumpingDur=9,
-        returningDur=10)
-
+    truck_id=1,
+    loading_dur=15,
+    hauling_dur=30,
+    dumping_dur=9,
+    returning_dur=10)
 
 a2 = Truck(
-        ID=2,
-        loadingDur=9,
-        haulingDur=15,
-        dumpingDur=11,
-        returningDur=5)
+    truck_id=2,
+    loading_dur=9,
+    hauling_dur=15,
+    dumping_dur=11,
+    returning_dur=5)
 
-
-myTruckList = [a0,a1,a2]
+myTruckList = [a0, a1, a2]
 quantity = len(myTruckList)
 
-myLoader=Loader("idle")
-
+myLoader = Loader("idle")
 
 for i in range(1):
-    activityData=[]
+    activityData = []
     realData = []
     for j in range(quantity):
-        singleBranchData = myTruckList[j].Process(BranchTime,myLoader)
+        singleBranchData = myTruckList[j].process(BranchTime, myLoader)
         activityData.append(singleBranchData)
-    
+
     # Priority list based on ES and EF
-    activityData = sorted(activityData , key=lambda k: [k[0], k[1]])
+    activityData = sorted(activityData, key=lambda k: [k[0], k[1]])
     print(activityData)
     for j in range(quantity):
-        singleRealData = myTruckList[activityData[j][2]].Process(SimulationTime,myLoader)
+        singleRealData = myTruckList[activityData[j][2]].process(SimulationTime, myLoader)
         realData.append(singleRealData)
     print(realData)
-        
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
