cacd04cd456d5247d1624073afc181f0241e9e29
Pedram Elmi <61834096+PedramElmi@users.noreply.github.com>
2020-11-27 16:04:14 +0330

    reformatted pep8

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from machine import Truck
 from programtools import Time
 from programtools import read_csv_truck_data
 
-# Crearte the main simulation time and branch simulation time
+# Create the main simulation time and branch simulation time
 branch = Time("branch")
 simulation = Time("simulation")
 
@@ -29,19 +29,20 @@ quantity_of_trucks = len(truck_list)
 the_loader = Loader(False, simulation.now)
 
 # write in "output-event-simulation-data.txt" kind of a incomplete logger file!
-with open("output-event-simulation-data.txt", 'w') as txtfile:
-
+with open("output-event-simulation-data.txt", 'w') as txt_file:
     # write starting details: simulation is running with these configs etc.
-    txtfile.write("*" * 80 + "\n")
-    txtfile.write(time.strftime(r'%Y-%m-%d %H:%M %Z',
-                                time.localtime(time.time())) + "\n")
-    txtfile.write("Event logs of the program from main.py" + "\n")
-    txtfile.write("-" * 80 + "\n")
-    txtfile.write("One loader machine is being created." + "\n")
-    txtfile.write("Created truck's list:" + "\n")
+    txt_file.write("*" * 80 + "\n")
+    txt_file.write(time.strftime(r'%Y-%m-%d %H:%M %Z',
+                                 time.localtime(time.time())) + "\n")
+    txt_file.write("Event logs of the program from main.py" + "\n")
+    txt_file.write("-" * 80 + "\n")
+    txt_file.write("One loader machine is being created." + "\n")
+    txt_file.write("Created truck's list:" + "\n")
     for truck in truck_list:
-        write_this = f"{truck.index} Truck ID={truck.id}: Status={truck.status}, Loading duration= {truck.loading_duration}, Hauling duration= {truck.hauling_duration}, Dumping duration= {truck.dumping_duration}, Returning duration= {truck.returning_duration}\n"
-        txtfile.write(write_this)
+        write_this = f"{truck.index} Truck ID={truck.id}: Status={truck.status}, " \
+                     f"Loading duration= {truck.loading_duration}, Hauling duration= {truck.hauling_duration}, " \
+                     f"Dumping duration= {truck.dumping_duration}, Returning duration= {truck.returning_duration}\n "
+        txt_file.write(write_this)
 
     # every discrete time in the sim will be stored here
     discrete_times = []
@@ -52,32 +53,33 @@ with open("output-event-simulation-data.txt", 'w') as txtfile:
     # Main Loop of the simulation for checking and doing EVERY available activity:
     for i in range(15):
 
-        # wrting with Discrete Event Simulation logics:
-        txtfile.write("- - " * 20 + "\n")
-        txtfile.write("Simulation time is now: {}\n".format(simulation.now))
+        # writing with Discrete Event Simulation logics:
+        txt_file.write("- - " * 20 + "\n")
+        txt_file.write("Simulation time is now: {}\n".format(simulation.now))
 
         # write loader's stats
-        txtfile.write("The loader is or will be available at time {}\nThe loader's working status is now {}. (True: Working, False: Idle).\n".format(
-            the_loader.available_time, the_loader.working))
+        txt_file.write("The loader is or will be available at time {}\n"
+                       "The loader's working status is now {}. "
+                       "(True: Working, False: Idle).\n".format(the_loader.available_time, the_loader.working))
 
         brunch_data = []
         real_data = []
-        # write truck'stats
+        # write truck's status
         for j in range(quantity_of_trucks):
             write_this = "Truck {} is at round {}. Its working status:{}".format(
                 truck_list[j].id, truck_list[j].round, truck_list[j].working)
-            txtfile.write(write_this + "\n")
+            txt_file.write(write_this + "\n")
 
         # reset branch to the main simulation before starting the branch
         branch.now = simulation.now
         write_this = "\nStarting a branch Simulation for prioritizing and checking doable activities:\n"
-        txtfile.write(write_this)
+        txt_file.write(write_this)
         # do a branch simulation of the main simulation for prioritizing and checking doable activities
         # call process method for all of the trucks
         for j in range(quantity_of_trucks):
             if not truck_list[j].working:
                 single_branch_data = truck_list[j].process(
-                    branch, the_loader, txtfile)
+                    branch, the_loader, txt_file)
                 brunch_data.append(single_branch_data)
 
         print(brunch_data)
@@ -85,28 +87,30 @@ with open("output-event-simulation-data.txt", 'w') as txtfile:
         # removing None values
         brunch_data = [k for k in brunch_data if k]
 
-        # Prioritize list based on EearlyStart and EearlyFinish
+        # Prioritize list based on Early StarIt and Eearly Finish
         brunch_data = sorted(brunch_data, key=lambda k: [k[0], k[1]])
 
-        write_this = "Branch Simulation has been compeleted. branch data of trucks (Sorted and Prioritized):\n[start, finish, ID, current status]\n"
-        txtfile.write(write_this)
+        write_this = "Branch Simulation has been compeleted. branch data of trucks (Sorted and Prioritized):\n" \
+                     "[start, finish, ID, current status]\n"
+        txt_file.write(write_this)
         for item in brunch_data:
-            txtfile.write("%s\n" % list(item))
+            txt_file.write("%s\n" % list(item))
         print(brunch_data)
 
         write_this = "\nStarting the main Simulation:\n"
-        txtfile.write(write_this)
+        txt_file.write(write_this)
         for j in brunch_data:
             single_real_data = truck_list[j[2]].process(
-                simulation, the_loader, txtfile)
+                simulation, the_loader, txt_file)
             real_data.append(single_real_data)
         print(real_data)
         real_data = [k for k in real_data if k]
 
-        write_this = "The main simulation is done. started truck's activities are:\n[start, finish, ID, current status]\n"
-        txtfile.write(write_this)
+        write_this = "The main simulation is done. started truck's activities are:\n" \
+                     "[start, finish, ID, current status]\n"
+        txt_file.write(write_this)
         for item in real_data:
-            txtfile.write("%s\n" % list(item))
+            txt_file.write("%s\n" % list(item))
 
         print(real_data)
 
